55150e7ebd4ad0b52e72ab134040bc99c09015af
hanna.um <oneme5797@gmail.com>
2023-07-25 18:08:33 +0900

    fix: company front 작업중

--- resources/js/layouts/company/CompanyLayout.tsx
+++ resources/js/layouts/company/CompanyLayout.tsx
@@ -2,8 +2,17 @@ import { PropsWithChildren, ReactNode } from 'react';
 
 import { User } from '@/types/user';
 
+import Header from './components/header';
+
 export default function CompanyLayout({
     children,
 }: PropsWithChildren<{ user: User; header?: ReactNode }>) {
-    return <div>{children}</div>;
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <div className="m-auto min-h-screen max-w-[435px] overflow-hidden bg-white">
+                <Header />
+                <div className="px-8 pt-8">{children}</div>
+            </div>
+        </div>
+    );
 }
---
+++ resources/js/layouts/company/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import Logo from '@assets/common/logo.svg';
+import IconMypage from '@assets/company/common/icon-mypage.svg';
+
+function Header() {
+    return (
+        <header className="shrink-0 shadow-[0_4px_20px_0_rgba(0,0,0,0.05)]">
+            <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+                <a href="/" className="-m-1.5 p-1.5">
+                    <img className="h-8 w-auto" src={Logo} alt="위드닥" />
+                </a>
+                <div className="flex items-center gap-x-8">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <img className="h-6 w-auto" src={IconMypage} alt="마이페이지" />
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
