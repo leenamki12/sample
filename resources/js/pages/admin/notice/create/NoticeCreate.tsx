76dda6d0cddcdc8940b11c27770c768701dd40ef
daegyun-moon <dgmoon@withbrother.co.kr>
2024-03-07 18:45:42 +0900

    [FEAT] 공지 게시 기능 추가

--- resources/js/pages/admin/notice/create/NoticeCreate.tsx
+++ resources/js/pages/admin/notice/create/NoticeCreate.tsx
@@ -1,19 +1,93 @@
+import { useRef, useEffect, useState } from 'react';
 import { Form, Input, Switch, Button } from 'antd';
-import ReactQuill from 'react-quill'; // Quill Editor import
-
-import 'react-quill/dist/quill.snow.css'; // Quill Editor 스타일 import
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import axios from 'axios';
+import { router } from '@inertiajs/react';
 
 interface NoticeFormData {
     title: string;
     content: string;
-    isMainVisible: boolean;
-    isMenuVisible: boolean;
+    isPublished: boolean;
+    isMainPublished: boolean;
+    file_ids: number[];
 }
 
 function NoticeCreate() {
-    const onFinish = (values: NoticeFormData) => {
-        console.log('Received values:', values);
+    const [fileIds, setFileIds] = useState<number[]>([]);
+
+    const quillRef = useRef(null);
+    const toolbarOptions = [
+        [{ header: [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['link', 'image', 'video'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+    ];
+
+    const modules = {
+        toolbar: {
+            container: toolbarOptions,
+        },
     };
+
+    const onFinish = async (values: NoticeFormData) => {
+        try {
+            const formData = {
+                type: 'NOTICE',
+                title: values.title,
+                content: values.content,
+                is_main_published: values.isMainPublished == true,
+                is_published: values.isPublished == true,
+                file_ids: fileIds,
+            };
+            console.log(formData);
+            router.post(route('admin.notice.store'), formData);
+        } catch (error) {
+            console.error('Error saving notice:', error);
+        }
+    };
+
+    useEffect(() => {
+        const editor = quillRef.current.getEditor();
+        const handleImage = () => {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.click();
+
+            input.onchange = async () => {
+                const file = input.files[0];
+                const range = editor.getSelection(true);
+                editor.insertEmbed(
+                    range.index,
+                    'image',
+                    `https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif`
+                );
+                try {
+                    const formData = new FormData();
+                    formData.append('image', file);
+                    const response = await axios.post(route('admin.notice.upload'), formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    });
+
+                    setFileIds(prevIds => [...prevIds, response.data.file_id]);
+                    editor.deleteText(range.index, 1);
+                    editor.insertEmbed(range.index, 'image', response.data.url);
+                    editor.setSelection(range.index + 1);
+                } catch (e) {
+                    console.error(e);
+                    editor.deleteText(range.index, 1);
+                }
+            };
+        };
+
+        if (quillRef.current) {
+            const toolbar = editor.getModule('toolbar');
+            toolbar.addHandler('image', handleImage);
+        }
+    }, []);
 
     return (
         <div style={{ margin: 'auto', padding: '20px' }}>
@@ -37,15 +111,23 @@ function NoticeCreate() {
                     name="content"
                     rules={[{ required: true, message: '공지내용을 입력해주세요.' }]}
                 >
-                    {/* Quill Editor로 변경 */}
-                    <ReactQuill theme="snow" placeholder="공지내용을 입력해주세요." />
+                    <ReactQuill
+                        ref={quillRef}
+                        theme="snow"
+                        modules={modules}
+                        style={{
+                            height: '400px',
+                            marginBottom: '50px',
+                        }}
+                        placeholder="공지내용을 입력해주세요."
+                    />
                 </Form.Item>
 
-                <Form.Item label="메인 노출여부" name="isMainVisible" valuePropName="checked">
+                <Form.Item label="메인 노출여부" name="isMainPublished" valuePropName="checked">
                     <Switch />
                 </Form.Item>
 
-                <Form.Item label="메뉴 노출여부" name="isMenuVisible" valuePropName="checked">
+                <Form.Item label="메뉴 노출여부" name="isPublished" valuePropName="checked">
                     <Switch />
                 </Form.Item>
 
