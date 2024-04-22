64048839c37d4557c2cc4b08073a3de0ede29b30
daegyun-moon <dgmoon@withbrother.co.kr>
2024-04-22 10:43:30 +0900

    Revert "[FIX] 공연 완료 전 갤러리 임시 closed"
    This reverts commit 4d64d4983a7799808443db52f103866108b4b075.

--- routes/guest/gallery.php
+++ routes/guest/gallery.php
@@ -2,8 +2,9 @@
 
 use App\Http\Web\Client\GalleryController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 
-// Route::prefix('gallery')->group(function () {
-//     Route::get('/', [GalleryController::class, 'index'])->name('gallery');
-// });
+Route::prefix('gallery')->group(function () {
+    Route::get('/', [GalleryController::class, 'index'])->name('gallery');
+});
