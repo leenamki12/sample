71f5d75916f640b7df32c54063fada0110c48a53
daegyun-moon <dgmoon@withbrother.co.kr>
2024-04-22 10:43:15 +0900

    [FIX] 티켓 구매 링크 제거

--- resources/js/templates/guest/components/header/Header.tsx
+++ resources/js/templates/guest/components/header/Header.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid';
 import { Link, usePage } from '@inertiajs/react';
 
-import buyTicket from '@assets/common/img_top_buyticket.png';
 import Logo from '@assets/common/logo.png';
 
 import * as s from './Header.styled';
@@ -98,23 +97,6 @@ function Header() {
                             <Bars3Icon className="h-[32px] w-[32px]" />
                         )}
                     </s.AllMenuButton>
-                    <s.BuyTicketWrap>
-                        <img src={buyTicket} alt="" />
-                        <a
-                            href="https://ticket.melon.com/performance/index.htm?prodId=209446"
-                            target="blank"
-                            className="melon"
-                        >
-                            <div className="hidden">멜론에서 예매 하러 가기</div>
-                        </a>
-                        <a
-                            href="http://ticket.yes24.com/New/Perf/Detail/Detail.aspx?IdPerf=48649"
-                            target="blank"
-                            className="yes24"
-                        >
-                            <div className="hidden">yes24에서 예매 하러 가기</div>
-                        </a>
-                    </s.BuyTicketWrap>
                 </s.InnerBox>
             </s.Wrapper>
             <s.AllMenu isActive={isAllMenuOpen}>
